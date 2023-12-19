c4cd61092fe099b7fa84c37833ae78f68708e95a
MariaRacariu <90383391+MariaRacariu@users.noreply.github.com>
2023-12-19 14:54:23 +0100

    fetch popular movies

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,70 @@
-const API_KEY = "42c639fdc4397f26da841871b32ae48c";
+async function fetchPopularMovies() {
+  const optionsPopular = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MmM2MzlmZGM0Mzk3ZjI2ZGE4NDE4NzFiMzJhZTQ4YyIsInN1YiI6IjY1ODA0YmE3M2E0OGM1MmI1NWFmNjcwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1AXhb6eE2vNLZkgQ5oYtNUSvny3AwfvfdZTS8deVi44'
+    }
+  };
 
-const url = `https://api.themoviedb.org/3/movie/100?language=en-US&api_key=${API_KEY}`;
+  const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
 
-fetch(url)
-  .then((res) => res.json())
-  .then((json) => console.log(json))
-  .catch((err) => console.error("error:" + err));
+  try {
+    const response = await fetch(url, optionsPopular);
+    console.log(response);
+
+    if (response.ok) {
+      const data = await response.json();
+      showPopularMovies(data);
+    } else if (response.status === 404) {
+      throw 404;
+    } else {
+      throw 'error';
+    }
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+fetchPopularMovies();
+
+function showPopularMovies(popularMovies) {
+  console.log(popularMovies);
+  for (const movie of popularMovies) {
+    const heading = document.createElement('h1');
+    document.body.append(heading);
+    const movieTitle = movie.original_title;
+    heading.innerText = movieTitle;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MmM2MzlmZGM0Mzk3ZjI2ZGE4NDE4NzFiMzJhZTQ4YyIsInN1YiI6IjY1ODA0YmE3M2E0OGM1MmI1NWFmNjcwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1AXhb6eE2vNLZkgQ5oYtNUSvny3AwfvfdZTS8deVi44'
+//   }
+// };
+//
+// fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
+
